73e958d0fbf126e604d04d4bf662633988597b6b
ronya <ron.yalensky@gmail.com>
2023-04-11 11:40:06 +0300

    Extracted SampleBag to an external component successfully

--- src/Components/SampleBag.jsx
+++ src/Components/SampleBag.jsx
@@ -1,34 +1,36 @@
-import {ErrorMessage, Field} from "formik";
 import React from "react";
+import { Field, ErrorMessage } from "formik";
 
-const SampleBag = ({bagNumber, index}) => (
-    <div className="sample-bag">
-        <h4>Sample Bag {bagNumber}</h4>
-        <div className="form-group">
-            <label htmlFor={`sampleBags.${index}.barcode`}>Barcode:</label>
-            <Field
-                type="text"
-                id={`sampleBags.${index}.barcode`}
-                name={`sampleBags.${index}.barcode`}
-            />
-            <ErrorMessage
-                className="error"
-                component="div"
-                name={`sampleBags.${index}.barcode`}
-            />
-        </div>
-        <div className="form-group">
-            <label htmlFor={`sampleBags.${index}.weight`}>Weight (grams):</label>
-            <Field
-                type="text"
-                id={`sampleBags.${index}.weight`}
-                name={`sampleBags.${index}.weight`}
-            />
-            <ErrorMessage
-                className="error"
-                component="div"
-                name={`sampleBags.${index}.weight`}
-            />
-        </div>
+const SampleBag = ({ bagNumber, index }) => (
+  <div className="sample-bag">
+    <h4>Sample Bag {bagNumber}</h4>
+    <div className="form-group">
+      <label htmlFor={`sampleBags.${index}.barcode`}>Barcode:</label>
+      <Field
+        type="text"
+        id={`sampleBags.${index}.barcode`}
+        name={`sampleBags.${index}.barcode`}
+      />
+      <ErrorMessage
+        className="error"
+        component="div"
+        name={`sampleBags.${index}.barcode`}
+      />
     </div>
+    <div className="form-group">
+      <label htmlFor={`sampleBags.${index}.weight`}>Weight (grams):</label>
+      <Field
+        type="text"
+        id={`sampleBags.${index}.weight`}
+        name={`sampleBags.${index}.weight`}
+      />
+      <ErrorMessage
+        className="error"
+        component="div"
+        name={`sampleBags.${index}.weight`}
+      />
+    </div>
+  </div>
 );
+
+export default SampleBag;
