7edca8404489640fe886df18764018bdfa893fdb
ronya <ron.yalensky@gmail.com>
2023-04-11 11:40:06 +0300

    Extracted SampleBag to an external component successfully

--- src/Components/SampleForm.jsx
+++ src/Components/SampleForm.jsx
@@ -4,6 +4,8 @@ import React from "react";
 import {Formik, Form, Field, FieldArray, ErrorMessage} from "formik";
 import * as Yup from "yup";
 import "../Styles/SampleFormStyle.css";
+import SampleBag from "./SampleBag";
+
 
 const initialValues = {
     seedsWeight: "",
@@ -23,37 +25,37 @@ const validationSchema = Yup.object().shape({
 
 });
 
-const SampleBag = ({bagNumber, index}) => (
-    <div className="sample-bag">
-        <h4>Sample Bag {bagNumber}</h4>
-        <div className="form-group">
-            <label htmlFor={`sampleBags.${index}.barcode`}>Barcode:</label>
-            <Field
-                type="text"
-                id={`sampleBags.${index}.barcode`}
-                name={`sampleBags.${index}.barcode`}
-            />
-            <ErrorMessage
-                className="error"
-                component="div"
-                name={`sampleBags.${index}.barcode`}
-            />
-        </div>
-        <div className="form-group">
-            <label htmlFor={`sampleBags.${index}.weight`}>Weight (grams):</label>
-            <Field
-                type="text"
-                id={`sampleBags.${index}.weight`}
-                name={`sampleBags.${index}.weight`}
-            />
-            <ErrorMessage
-                className="error"
-                component="div"
-                name={`sampleBags.${index}.weight`}
-            />
-        </div>
-    </div>
-);
+// const SampleBag = ({bagNumber, index}) => (
+//     <div className="sample-bag">
+//         <h4>Sample Bag {bagNumber}</h4>
+//         <div className="form-group">
+//             <label htmlFor={`sampleBags.${index}.barcode`}>Barcode:</label>
+//             <Field
+//                 type="text"
+//                 id={`sampleBags.${index}.barcode`}
+//                 name={`sampleBags.${index}.barcode`}
+//             />
+//             <ErrorMessage
+//                 className="error"
+//                 component="div"
+//                 name={`sampleBags.${index}.barcode`}
+//             />
+//         </div>
+//         <div className="form-group">
+//             <label htmlFor={`sampleBags.${index}.weight`}>Weight (grams):</label>
+//             <Field
+//                 type="text"
+//                 id={`sampleBags.${index}.weight`}
+//                 name={`sampleBags.${index}.weight`}
+//             />
+//             <ErrorMessage
+//                 className="error"
+//                 component="div"
+//                 name={`sampleBags.${index}.weight`}
+//             />
+//         </div>
+//     </div>
+// );
 
 const SampleForm = () => {
     const [bagNumber, setBagNumber] = React.useState(1);
@@ -63,6 +65,7 @@ const SampleForm = () => {
             return totalWeight + parseFloat(bag.weight || 0);
         }, 0);
     }
+
     return (
         <div className="form-container">
             <h2>Sample Form</h2>
@@ -88,6 +91,48 @@ const SampleForm = () => {
                             />
                         </div>
 
+                        {/*<FieldArray name="sampleBags">*/}
+                        {/*    {({insert, remove, push}) => (*/}
+                        {/*        <>*/}
+                        {/*            {values.sampleBags.length > 0 &&*/}
+                        {/*                values.sampleBags.map((sampleBag, index) => (*/}
+                        {/*                    <div key={index}>*/}
+                        {/*                        <SampleBag*/}
+                        {/*                            index={index}*/}
+                        {/*                            bagNumber={sampleBag.bagNumber}*/}
+                        {/*                        />*/}
+                        {/*                        <button*/}
+                        {/*                            type="button"*/}
+                        {/*                            className="btn remove"*/}
+                        {/*                            onClick={() => {*/}
+                        {/*                                remove(index);*/}
+                        {/*                                //TODO check what to do if remove*/}
+                        {/*                                // setBagNumber((prevBagNumber) => prevBagNumber -1);*/}
+                        {/*                            }}*/}
+                        {/*                        >*/}
+                        {/*                            Remove*/}
+                        {/*                        </button>*/}
+                        {/*                    </div>*/}
+                        {/*                ))}*/}
+                        {/*            <button*/}
+                        {/*                type="button"*/}
+                        {/*                className="btn add"*/}
+                        {/*                //TODO - check this*/}
+
+                        {/*                disabled={*/}
+                        {/*                    getTotalSampleBagWeight(values.sampleBags) >= values.seedsWeight &&*/}
+                        {/*                    values.sampleBags.length > 0*/}
+                        {/*                }*/}
+                        {/*                onClick={() => {*/}
+                        {/*                    push({barcode: "", weight: "", bagNumber});*/}
+                        {/*                    setBagNumber((prevBagNumber) => prevBagNumber + 1);*/}
+                        {/*                }}*/}
+                        {/*            >*/}
+                        {/*                Add Sample Bag*/}
+                        {/*            </button>*/}
+                        {/*        </>*/}
+                        {/*    )}*/}
+                        {/*</FieldArray>*/}
                         <FieldArray name="sampleBags">
                             {({insert, remove, push}) => (
                                 <>
